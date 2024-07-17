541b947c033c9148bf5437a09b62382d2dedcdc0
Abiola Bolaji <ab2113@princeton.edu>
2024-07-17 16:41:27 -0400

    j

--- taint_tracking/riscv_crypto_fu_saes64_taint.tcl
+++ taint_tracking/riscv_crypto_fu_saes64_taint.tcl
@@ -8,7 +8,7 @@ reset g_resetn -non_resettable_regs 0
 
 # check once the computation is finsihed that there is no 
 # information leak to the registar where data is stored
-assume {rs1_t == 0 && valid-t == 0}
+assume {rs1_t == 0 && valid_t == 0}
 assert {rd_t == 0}
 
 
