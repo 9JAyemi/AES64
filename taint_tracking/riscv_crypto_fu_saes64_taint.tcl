2499582b4247dafa346a707dd990e655dc8d07f7
Abiola Bolaji <ab2113@princeton.edu>
2024-07-17 16:41:02 -0400

    j

--- taint_tracking/riscv_crypto_fu_saes64_taint.tcl
+++ taint_tracking/riscv_crypto_fu_saes64_taint.tcl
@@ -8,8 +8,8 @@ reset g_resetn -non_resettable_regs 0
 
 # check once the computation is finsihed that there is no 
 # information leak to the registar where data is stored
-assume {rs1_t == 0}
-assert {valid_t == 0}
+assume {rs1_t == 0 && valid-t == 0}
+assert {rd_t == 0}
 
 
 # Set the time limit to 1 hour (3600 seconds)
