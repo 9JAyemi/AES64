eb5d7e4c80c72256167cc28ab73670195045ed9a
Abiola Bolaji <ab2113@princeton.edu>
2024-07-23 18:14:28 -0400

    d

--- spv_aes64.tcl
+++ spv_aes64.tcl
@@ -9,7 +9,7 @@ stopat result_ks1
 stopat result_ks2
 # stopat result_enc
 # stopat result_dec
-# stopat result_imix
+stopat result_imix
 stopat sb_fwd_out
 stopat sb_inv_out
 # check_spv -create -from {valid rs1 rs2 enc_rcon op_saes64_ks1 op_saes64_ks2 op_saes64_imix op_saes64_encs op_saes64_encsm op_saes64_decs op_saes64_decsm} -to ready
