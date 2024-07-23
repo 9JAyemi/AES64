b1d09c84eef2c137e9316f3f9ab78ba6409ab6b1
Abiola Bolaji <ab2113@princeton.edu>
2024-07-23 18:12:36 -0400

    g

--- spv_aes64.tcl
+++ spv_aes64.tcl
@@ -5,16 +5,18 @@ elaborate -top riscv_crypto_fu_saes64
 clock g_clk
 reset g_resetn -non_resettable_regs 0
 
-stopat result_ks1
-stopat result_ks2
-stopat result_enc
-stopat result_dec
-stopat result_imix
+# stopat result_ks1
+# stopat result_ks2
+# stopat result_enc
+# stopat result_dec
+# stopat result_imix
+stopat sb_fwd_out
+stopat sb_inv_out
 # check_spv -create -from {valid rs1 rs2 enc_rcon op_saes64_ks1 op_saes64_ks2 op_saes64_imix op_saes64_encs op_saes64_encsm op_saes64_decs op_saes64_decsm} -to ready
 # check_spv -create -from {valid rs1 rs2 enc_rcon op_saes64_ks1 op_saes64_ks2 op_saes64_imix op_saes64_encs op_saes64_encsm op_saes64_decs op_saes64_decsm} -to rd
 
-check_spv -create -from {rs1} -to ready
-check_spv -create -from {rs1} -to rd
+check_spv -create -from {rs1 rs2} -to ready
+check_spv -create -from {rs1 rs2} -to rd
 
 
 
