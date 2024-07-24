0ff110ee86ad2b2137865bd360d9f69e5d1e648f
Abiola Bolaji <ab2113@princeton.edu>
2024-07-23 21:18:34 -0400

    d

--- selfcomp_aes64.tcl
+++ selfcomp_aes64.tcl
@@ -4,7 +4,9 @@ elaborate -top SAES64_Tester
 
 clock g_clk
 reset g_resetn -non_resettable_regs 0
-
+stopat rd1
+stopat rd2
+assume {rd1 == rd2}
 assert {oneReady -> bothReady}
 assert {bothReady -> rd1 == rd2}
 
